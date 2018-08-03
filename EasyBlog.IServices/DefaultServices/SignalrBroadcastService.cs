8a8c0393e05b5fc69400dac7d8021289e952da8a
lv-conner <lvmh123456@gmail.com>
2018-08-03 08:51:32 +0800

    signalr

--- EasyBlog.IServices/DefaultServices/SignalrBroadcastService.cs
+++ EasyBlog.IServices/DefaultServices/SignalrBroadcastService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.SignalR;
 namespace EasyBlog.Services.DefaultServices
 {
     public class SignalrBroadcastService : IBroadcastService
---
+++ EasyBlog/SignalrBroadcastService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace EasyBlog
+{
+    public class SignalrBroadcastService<THub> where THub:Hub
+    {
+        private readonly HubLifetimeManager<THub> _hubManager;
+        public SignalrBroadcastService(HubLifetimeManager<THub> hubManager)
+        {
+            _hubManager = hubManager;
+        }
+    }
+}
